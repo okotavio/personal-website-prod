32be13dbf8d66cb066d70db02507ce0a85cd7475
Otavio Vidal <otavioluisvidal@gmail.com>
2022-10-23 18:51:07 +0200

    reverted favicon const to simpler version

--- components/layout.js
+++ components/layout.js
@@ -5,23 +5,12 @@ import Footer from "./footer";
 import styles from "../styles/Home.module.css";
 
 export default function Layout({ children, pageTitle }) {
-  const [faviconVar, setFaviconVar] = useState(
-    Math.floor(Math.random() * 3) + 1
-  );
-
-  useEffect(() => {
-    if (faviconVar === 3) {
-      setFaviconVar(Math.floor(Math.random() * 2) + 1);
-    } else {
-      setFaviconVar(faviconVar + 1);
-    }
-  }, []);
-
+  const faviconVar = Math.floor(Math.random() * 3) + 1;
   return (
     <>
       <Head>
         <title>
-          {pageTitle} {pageTitle ? "—" : ""} Otávio ✦ Product designer
+          {pageTitle} {pageTitle ? "—" : ""} Otávio ✦ Product Designer
         </title>
         <meta property="og:type" content="website" />
         <link rel="icon" href={`favicon-${faviconVar}.ico`} />
