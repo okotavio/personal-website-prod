2895ebf58cd8b3627d7496ff62ab8f7b2c55ba2a
Otavio Vidal <otavioluisvidal@gmail.com>
2022-10-28 22:04:39 +0200

    align header to the center and changed styles

--- components/header.js
+++ components/header.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ActiveLink from "../components/activelink";
 import { useRouter } from "next/router";
+import Logo from "../components/logo";
 
 export default function Header({}) {
   const router = useRouter();
   return (
-    <header>
-      <h1>This is the header</h1>
-      <nav className="menu">
-        <ActiveLink href="/">Home</ActiveLink>
-        <ActiveLink href="/about">About</ActiveLink>
-      </nav>
-    </header>
+    <div className="headercontainer">
+      <header>
+        <div className="logo">
+          <Logo />
+        </div>
+        <nav className="menu">
+          <ActiveLink href="/">Home</ActiveLink>
+          <ActiveLink href="/about">About</ActiveLink>
+        </nav>
+      </header>
+    </div>
   );
 }
