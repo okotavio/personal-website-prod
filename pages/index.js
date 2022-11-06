b97f9b13de5fbd0cd4977d187212f5ee5c1d5334
Otavio Vidal <otavioluisvidal@gmail.com>
2022-11-06 12:26:01 +0100

    added first iteration of hero content

--- pages/index.js
+++ pages/index.js
@@ -11,58 +11,18 @@ export default function Home() {
     <Layout>
       <main className={styles.main}>
         <Container>
-          <p>
-            During the 1970s, graphic designers were raiding style books and
-            publications of earlier generations looking for more funky stylings,
-            and found a treasure trove in the “Old Style” types from earlier in
-            the century. The irreverence and friendliness of these typefaces was
-            the perfect countercultural antidote to the perceived stuffiness and
-            chilly efficiency of traditional text faces. A prime example of this
-            style coming out of the ’70s was Ed Benguiat’s swashy version of
-            Bookman for ITC. Benguiat’s type was an ode to Oswald Cooper’s
-            Cooper Black Italic and is known to most people as the friendly,
-            “Thank You For Shopping With Us” (a typographical “wave” of
-            appreciation) that graces grocery bags all over North America.
-          </p>
-          <p>
-            During the 1970s, graphic designers were raiding style books and
-            publications of earlier generations looking for more funky stylings,
-            and found a treasure trove in the “Old Style” types from earlier in
-            the century. The irreverence and friendliness of these typefaces was
-            the perfect countercultural antidote to the perceived stuffiness and
-            chilly efficiency of traditional text faces. A prime example of this
-            style coming out of the ’70s was Ed Benguiat’s swashy version of
-            Bookman for ITC. Benguiat’s type was an ode to Oswald Cooper’s
-            Cooper Black Italic and is known to most people as the friendly,
-            “Thank You For Shopping With Us” (a typographical “wave” of
-            appreciation) that graces grocery bags all over North America.
-          </p>
-          <p>
-            During the 1970s, graphic designers were raiding style books and
-            publications of earlier generations looking for more funky stylings,
-            and found a treasure trove in the “Old Style” types from earlier in
-            the century. The irreverence and friendliness of these typefaces was
-            the perfect countercultural antidote to the perceived stuffiness and
-            chilly efficiency of traditional text faces. A prime example of this
-            style coming out of the ’70s was Ed Benguiat’s swashy version of
-            Bookman for ITC. Benguiat’s type was an ode to Oswald Cooper’s
-            Cooper Black Italic and is known to most people as the friendly,
-            “Thank You For Shopping With Us” (a typographical “wave” of
-            appreciation) that graces grocery bags all over North America.
-          </p>
-          <p>
-            During the 1970s, graphic designers were raiding style books and
-            publications of earlier generations looking for more funky stylings,
-            and found a treasure trove in the “Old Style” types from earlier in
-            the century. The irreverence and friendliness of these typefaces was
-            the perfect countercultural antidote to the perceived stuffiness and
-            chilly efficiency of traditional text faces. A prime example of this
-            style coming out of the ’70s was Ed Benguiat’s swashy version of
-            Bookman for ITC. Benguiat’s type was an ode to Oswald Cooper’s
-            Cooper Black Italic and is known to most people as the friendly,
-            “Thank You For Shopping With Us” (a typographical “wave” of
-            appreciation) that graces grocery bags all over North America.
-          </p>
+          <div className="hero">
+            <div className="title">
+              <h1>Product,</h1>
+              <h1>Systems,</h1>
+              <h1 className="last-name">Designer.</h1>
+            </div>
+            <p className="hero-description">
+              Brazilian-born product designer crafting systems and experiences
+              in Berlin, Germany. Previously a Senior Product Designer at
+              Hopper, Delivery Hero and QuintoAndar.
+            </p>
+          </div>
         </Container>
       </main>
     </Layout>
