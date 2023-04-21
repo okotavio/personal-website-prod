f4564fc99f6b569c7a4b6108c3c12d1ec5d46b8a
okotavio <otavioluisvidal@gmail.com>
2023-04-21 12:51:18 +0200

    fixed analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import "nprogress/nprogress.css"; //styles of nprogress
 import "../styles/globals.scss";
 import "../styles/theme.scss";
 import Script from "next/script";
+import * as gtag from "../lib/gtag";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
@@ -11,25 +12,35 @@ Router.events.on("routeChangeError", () => NProgress.done());
 NProgress.configure({ showSpinner: false });
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
       <Script
         strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-LT46QNLE26"
-      />
+      ></Script>
           
       <Script
         id="google-analytics"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-LT46QNLE26', {
-            page_path: window.location.pathname,
-          });
-        `,
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-LT46QNLE26', {
+                page_path: window.location.pathname,
+              });
+            `,
         }}
       />
       <Component {...pageProps} />;
