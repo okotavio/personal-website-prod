ad68a9c249866f33a52ae69a651b40b9408f645b
okotavio <otavioluisvidal@gmail.com>
2023-04-21 12:38:33 +0200

    added google analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import NProgress from "nprogress"; //nprogress module
 import "nprogress/nprogress.css"; //styles of nprogress
 import "../styles/globals.scss";
 import "../styles/theme.scss";
+import Script from "next/script";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
@@ -10,7 +11,30 @@ Router.events.on("routeChangeError", () => NProgress.done());
 NProgress.configure({ showSpinner: false });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-LT46QNLE26"
+      />
+          
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-LT46QNLE26', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
