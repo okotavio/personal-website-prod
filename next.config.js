0e12d4fe01008bf3aa49349f4ce9871b19945697
Otavio Luis Vidal <45403524+okotavio@users.noreply.github.com>
2025-04-12 14:34:14 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/portfolio-2025',
+        source: '/portfolio-ds-2025',
         destination: 'https://www.figma.com/deck/nyyDGUGkhCoh9N4n1NXdS2/Otavio-Vidal-%7C-2025-Portfolio?node-id=2017-15572&viewport=-1819%2C-5803%2C0.85&t=e7G06hKHiYh7gUxF-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1',
         permanent: false, // use true if the redirect is final and won't change
       },
