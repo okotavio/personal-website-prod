c1793993a5329372104f56ce8e431335ead8474e
Otavio Luis Vidal <45403524+okotavio@users.noreply.github.com>
2025-04-12 14:32:20 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/portfolio-2025',
+        destination: 'https://www.figma.com/deck/nyyDGUGkhCoh9N4n1NXdS2/Otavio-Vidal-%7C-2025-Portfolio?node-id=2017-15572&viewport=-1819%2C-5803%2C0.85&t=e7G06hKHiYh7gUxF-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1',
+        permanent: false, // use true if the redirect is final and won't change
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
